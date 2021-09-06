9cfbdf8904d235413293c105b04e117bda1f06ec
Parmeshwar Rathod <77828508+parmeshwar1713@users.noreply.github.com>
2021-09-06 12:13:21 +0530

    Add files via upload

---
+++ MYDASH/MYDASH/urls.py
@@ -0,0 +1,32 @@
+"""MYDASH URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls import url
+from mydash1 import views
+from django.views.generic import TemplateView
+urlpatterns = [
+    path('', views.index1, name='dev'),
+    # path('mydash1/', views.index, name='dev'),
+    path('smp/', views.simple_uload, name='dev1'),
+   
+    # path('pl/', views.pl, name='plot'),
+    
+    path('admin/', admin.site.urls),
+    path('django_plotly_dash/', include('django_plotly_dash.urls')),
+    path('dash_plot/', TemplateView.as_view(template_name='tempt.html'), name="dash_plot"),
+    
+]
